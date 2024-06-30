b41709c45bd09adcb12dfb5a7c4da78189a853c4
VedantG479 <gargvedant2004@gmail.com>
2024-06-30 17:23:12 +0530

    following not following backend fix

--- app/src/main/java/com/example/growwth/fragments/Roadmaps.kt
+++ app/src/main/java/com/example/growwth/fragments/Roadmaps.kt
@@ -1,6 +1,7 @@
 package com.example.growwth.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.growwth.FillRoadmap
 import com.example.growwth.FillRoadmapAdapter
 import com.example.growwth.R
 import com.google.firebase.auth.FirebaseAuth
@@ -64,6 +64,7 @@ class Roadmaps : Fragment() {
 
                 for (data in snapshot.children) {
                     val roadmapName = data.child("name").getValue(String::class.java) ?: ""
+                    Log.d("Roadmap:", roadmapName)
                     roadmaps.add(roadmapName)
                 }
 
